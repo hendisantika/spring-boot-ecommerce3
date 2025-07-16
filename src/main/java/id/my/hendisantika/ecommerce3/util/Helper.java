73ec88e5c46c9af254eba16b88d3fe7ba3534a2e
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:51:40 +0700

    feat: Add getCounts in Helper🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerce3/util/Helper.java
+++ src/main/java/id/my/hendisantika/ecommerce3/util/Helper.java
@@ -1,5 +1,12 @@
 package id.my.hendisantika.ecommerce3.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-ecommerce3
@@ -25,5 +32,26 @@ public class Helper {
         } else {
             return (desc + "...");
         }
+    }
+
+    public static Map<String, Long> getCounts(SessionFactory factory) {
+        Session session = factory.openSession();
+        String q1 = "Select count(*) from User";
+        String q2 = "Select count(*) from Product";
+
+        Query query1 = session.createQuery(q1);
+        Query query2 = session.createQuery(q2);
+
+        Long userCount = (Long) query1.list().get(0);
+        Long productCount = (Long) query2.list().get(0);
+
+        Map<String, Long> map = new HashMap<String, Long>();
+        map.put("userCount", userCount);
+        map.put("productCount", productCount);
+
+
+        session.close();
+
+        return map;
     }
 }
