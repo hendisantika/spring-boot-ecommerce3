984a9d8050f7accdbebd6567a807b464bd8129b1
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 06:02:00 +0700

    feat: Add addProductForm in ProductController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerce3/controller/ProductController.java
+++ src/main/java/id/my/hendisantika/ecommerce3/controller/ProductController.java
@@ -2,8 +2,11 @@ package id.my.hendisantika.ecommerce3.controller;
 
 import id.my.hendisantika.ecommerce3.repository.CategoryRepository;
 import id.my.hendisantika.ecommerce3.repository.ProductRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,4 +28,14 @@ public class ProductController {
     private final CategoryRepository categoryRepository;
 
     private final ProductRepository productRepository;
+
+    @GetMapping("/add")
+    public String addProductForm(Model model, HttpSession session) {
+        if (session.getAttribute("current-user") == null) {
+            return "redirect:/login";
+        }
+
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "add_product";
+    }
 }
