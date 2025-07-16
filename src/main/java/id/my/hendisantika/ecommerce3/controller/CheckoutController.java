b687a5b0ca9bc78902a7f8ba7c53bb76585920d8
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:58:57 +0700

    feat: Add checkoutPage in CheckoutController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerce3/controller/CheckoutController.java
+++ src/main/java/id/my/hendisantika/ecommerce3/controller/CheckoutController.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.ecommerce3.controller;
 
+import id.my.hendisantika.ecommerce3.entity.Category;
 import id.my.hendisantika.ecommerce3.repository.CategoryRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CheckoutController {
 
     private final CategoryRepository categoryRepository;
+
+    @GetMapping
+    public String checkoutPage(Model model, HttpSession session) {
+        // Get categories for navbar
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+
+        return "checkout";
+    }
 }
