c59180a790002fa88f71235111fff397f7cdab0b
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:55:48 +0700

    feat: Add adminDashboard in AdminController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerce3/controller/AdminController.java
+++ src/main/java/id/my/hendisantika/ecommerce3/controller/AdminController.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.ecommerce3.controller;
 
+import id.my.hendisantika.ecommerce3.entity.User;
 import id.my.hendisantika.ecommerce3.repository.CategoryRepository;
 import id.my.hendisantika.ecommerce3.repository.ProductRepository;
 import id.my.hendisantika.ecommerce3.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -28,4 +32,26 @@ public class AdminController {
     private final ProductRepository productRepository;
 
     private final UserRepository userRepository;
+
+    @GetMapping
+    public String adminDashboard(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("current-user");
+
+        // Check if user is admin
+        if (user == null || !user.getUserType().equals("admin")) {
+            return "redirect:/login";
+        }
+
+        // Count of users, categories, and products
+        long userCount = userRepository.count();
+        long categoryCount = categoryRepository.count();
+        long productCount = productRepository.count();
+
+        model.addAttribute("userCount", userCount);
+        model.addAttribute("categoryCount", categoryCount);
+        model.addAttribute("productCount", productCount);
+        model.addAttribute("categories", categoryRepository.findAll());
+
+        return "admin";
+    }
 }
