5079f5830d81ce5feb213b0beeb126d222b2f31f
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:33:32 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerce3/SpringBootEcommerce3Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.ecommerce3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerce3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEcommerce3Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/ecommerce3/SpringBootEcommerce3ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.ecommerce3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerce3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
