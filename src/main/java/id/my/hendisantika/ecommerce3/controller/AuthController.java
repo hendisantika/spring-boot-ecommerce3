fd95bd0417d9b4c3889ec8ae2213cdcc4d5cd5c4
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:56:27 +0700

    feat: Add AuthController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerce3/controller/AuthController.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.ecommerce3.controller;
+
+import id.my.hendisantika.ecommerce3.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce3
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/07/25
+ * Time: 05.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+
+}
