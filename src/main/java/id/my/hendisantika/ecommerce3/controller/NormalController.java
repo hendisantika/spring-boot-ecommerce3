e60de8228b9891846c6f15999b86751df2689676
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 06:01:13 +0700

    feat: Add normalDashboard in NormalController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerce3/controller/NormalController.java
+++ src/main/java/id/my/hendisantika/ecommerce3/controller/NormalController.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.ecommerce3.controller;
 
+import id.my.hendisantika.ecommerce3.entity.Category;
+import id.my.hendisantika.ecommerce3.entity.User;
 import id.my.hendisantika.ecommerce3.repository.CategoryRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +29,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class NormalController {
 
     private final CategoryRepository categoryRepository;
+
+    @GetMapping
+    public String normalDashboard(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("current-user");
+
+        // Check if user is normal user
+        if (user == null || !user.getUserType().equals("normal")) {
+            return "redirect:/login";
+        }
+
+        // Get categories for navbar
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+
+        return "normal";
+    }
 }
