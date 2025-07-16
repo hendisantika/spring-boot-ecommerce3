a56705c245fae0f303b3905d99f75a7be527630d
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:59:39 +0700

    feat: Add HomeController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerce3/controller/HomeController.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.ecommerce3.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce3
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/07/25
+ * Time: 05.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class HomeController {
+}
