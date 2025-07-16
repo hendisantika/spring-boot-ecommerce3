197b5a34539d104a3646b9d3e06f9c734911af0b
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:48:10 +0700

    feat: Add CustomUserDetailsService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerce3/service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.ecommerce3.service;
+
+import id.my.hendisantika.ecommerce3.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce3
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/07/25
+ * Time: 05.47
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // In our application, we're using email as the username
+        return userRepository.findByUserEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+    }
+
+}
