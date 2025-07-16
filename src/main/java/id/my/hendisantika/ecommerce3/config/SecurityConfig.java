5d9fb620eb8e6c863dcdf758123a904e5cdc4bc0
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:38:15 +0700

    feat: Add authenticationProvider in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerce3/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/ecommerce3/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.ecommerce3.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,5 +33,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
