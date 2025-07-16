c63ee58472b1fa8c93913b4c61df5a32a5b49128
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 05:38:02 +0700

    feat: Add passwordEncoder in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerce3/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/ecommerce3/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package id.my.hendisantika.ecommerce3.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +29,8 @@ public class SecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
